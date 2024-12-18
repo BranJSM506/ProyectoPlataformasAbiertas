57871af37b462269eca3e64030520ba426a98eeb
BranJSM506 <149101596+BranJSM506@users.noreply.github.com>
2024-12-18 13:17:46 -0600

    Update and rename MarcasController.php to MarcaController.php

--- src/controllers/MarcaController.php
+++ src/controllers/MarcaController.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../models/Marca.php';
+require_once __DIR__ . '/../module/Marca.php';
 
 class MarcaController {
     private $model;
@@ -87,5 +87,4 @@ public function put($id) {
     }
 }
 ?>
-
 
